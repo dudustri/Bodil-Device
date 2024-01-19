58b27bf7d51c811789ede01a64d5e128ee98343e
Eduardo <eduardostrindade@gmail.com>
2024-01-19 11:48:06 +0100

    feat(gsm): creating a netif to integrate the gsm module using PPP protocol

--- main/wifi_connection.h
+++ main/wifi_connection.h
@@ -1,6 +1,13 @@
 #ifndef WIFI_CONNECTION_H
 #define WIFI_CONNECTION_H
 
+#include "esp_log.h"
+#include "esp_event.h"
+#include "esp_wifi.h"
+#include "lwip/err.h"
+#include "lwip/sys.h"
+#include "lwip/ip4_addr.h"
+
 extern int retry_conn_num; 
 
 void wifi_connection_init();
